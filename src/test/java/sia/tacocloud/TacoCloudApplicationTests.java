7b2593f9b2a935f1023916c086447a60d4a452be
Aleksandr Belov <uka_ka@inbox.ru>
2023-09-27 20:06:09 +0300

    Initial commit

---
+++ src/test/java/sia/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,15 @@
+package sia.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//настраивает junit на запуск теста с поддержкой возможностей спринг бут
+//выполняет загрузку контекста приложения спринг для теста
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
